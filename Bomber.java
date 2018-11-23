728b5ea16b9fc18b78eda03975afb0324ef3e057
thangnmuet2017 <43413114+thangnmuet2017@users.noreply.github.com>
2018-11-23 17:58:24 +0700

    Update Bomber

--- Bomber.java
+++ Bomber.java
@@ -3,19 +3,18 @@ package uet.oop.bomberman.entities.character;
 import uet.oop.bomberman.Board;
 import uet.oop.bomberman.Game;
 import uet.oop.bomberman.entities.Entity;
-import uet.oop.bomberman.entities.LayeredEntity;
 import uet.oop.bomberman.entities.bomb.Bomb;
-import uet.oop.bomberman.entities.bomb.Flame;
-import uet.oop.bomberman.entities.character.enemy.Enemy;
-import uet.oop.bomberman.entities.tile.Grass;
-import uet.oop.bomberman.entities.tile.Wall;
 import uet.oop.bomberman.graphics.Screen;
 import uet.oop.bomberman.graphics.Sprite;
 import uet.oop.bomberman.input.Keyboard;
-import uet.oop.bomberman.level.Coordinates;
 
 import java.util.Iterator;
 import java.util.List;
+import uet.oop.bomberman.entities.LayeredEntity;
+import uet.oop.bomberman.entities.bomb.Flame;
+import uet.oop.bomberman.entities.character.enemy.Enemy;
+import uet.oop.bomberman.entities.tile.Wall;
+import uet.oop.bomberman.level.Coordinates;
 
 public class Bomber extends Character {
 
@@ -24,7 +23,7 @@ public class Bomber extends Character {
 
     /**
      * nếu giá trị này < 0 thì cho phép đặt đối tượng Bomb tiếp theo,
-     * cứ mỗi lần đặt 1 Bomb mới, giá trị này sẽ được reset về 0 và giảm dần trong mỗi lần update()
+     * cứ mỗi lần đặt 1 Bomb mới, giá trị này sẽ được reset 0 và giảm dần trong mỗi lần update()
      */
     protected int _timeBetweenPutBombs = 0;
 
@@ -32,7 +31,7 @@ public class Bomber extends Character {
         super(x, y, board);
         _bombs = _board.getBombs();
         _input = _board.getInput();
-        _sprite = Sprite.player_right;
+        _sprite = Sprite.player_right; // _sprite trong Entity
     }
 
     @Override
@@ -74,30 +73,28 @@ public class Bomber extends Character {
      * Kiểm tra xem có đặt được bom hay không? nếu có thì đặt bom tại vị trí hiện tại của Bomber
      */
     private void detectPlaceBomb() {
-        // TODO: kiểm tra xem phím điều khiển đặt bom có được gõ và giá trị _timeBetweenPutBombs, Game.getBombRate() có thỏa mãn hay không
-        // TODO:  Game.getBombRate() sẽ trả về số lượng bom có thể đặt liên tiếp tại thời điểm hiện tại
-        // TODO: _timeBetweenPutBombs dùng để ngăn chặn Bomber đặt 2 Bomb cùng tại 1 vị trí trong 1 khoảng thời gian quá ngắn
-        // TODO: nếu 3 điều kiện trên thỏa mãn thì thực hiện đặt bom bằng placeBomb()
-        // TODO: sau khi đặt, nhớ giảm số lượng Bomb Rate và reset _timeBetweenPutBombs về 0
-        if(_input.space && Game.getBombRate() > 0 && _timeBetweenPutBombs < 0) {
-
-            int xt = Coordinates.pixelToTile(_x + _sprite.getSize()/2);
-            int yt = Coordinates.pixelToTile(_y + _sprite.getSize()/2 - _sprite.getSize());
-
-            this.placeBomb(xt, yt);
-            //Sound.getIstance().getAudio(Sound.BOMB).play();
-
+        // TODO: kiểm tra xem phím đi�?u khiển đặt bom có được gõ và giá trị _timeBetweenPutBombs, Game.getBombRate() có th�?a mãn hay không
+        // TODO:  Game.getBombRate() sẽ trả v�? số lượng bom có thể đặt liên tiếp tại th�?i điểm hiện tại
+        // TODO: _timeBetweenPutBombs dùng để ngăn chặn Bomber đặt 2 Bomb cùng tại 1 vị trí trong 1 khoảng th�?i gian quá ngắn
+        // TODO: nếu 3 đi�?u kiện trên th�?a mãn thì thực hiện đặt bom bằng placeBomb()
+        // TODO: sau khi đặt, nhớ giảm số lượng Bomb Rate và reset _timeBetweenPutBombs v�? 0
+		if(_input.space && Game.getBombRate() > 0 && _timeBetweenPutBombs < 0) {
+			
+			// tinh tam vi tri hien tai theo toa do tile va dat bomb tai vi tri do
+			// cong thuc tinh toa do tam :
+            int xt = Coordinates.pixelToTile(_x + _sprite.getSize()/2); 
+			int yt = Coordinates.pixelToTile(_y - _sprite.getSize()/2); 
+			placeBomb(xt, yt);
+			// Trong Sprite, tat ca cac doi tuong deu co SIZE = 16, getSize() tra ve SIZE nen _sprite.getSize() = 16
             Game.addBombRate(-1);
-
-            _timeBetweenPutBombs = 30;
-
-
+            _timeBetweenPutBombs = 30
         }
+
     }
 
     protected void placeBomb(int x, int y) {
         // TODO: thực hiện tạo đối tượng bom, đặt vào vị trí (x, y)
-        Bomb b = new Bomb(x, y , _board);
+		Bomb b = new Bomb(x, y, _board);
         _board.addBomb(b);
     }
 
@@ -131,35 +128,33 @@ public class Bomber extends Character {
 
     @Override
     protected void calculateMove() {
-        // TODO: xử lý nhận tín hiệu điều khiển hướng đi từ _input và gọi move() để thực hiện di chuyển
-        int xa =0, ya =0;
+        // TODO: xử lý nhận tín hiệu đi�?u khiển hướng đi từ _input và g�?i move() để thực hiện di chuyển
+        // TODO: nhớ cập nhật lại giá trị c�? _moving khi thay đổi trạng thái di chuyển
+		int xa =0, ya =0;
         if(_input.up)   ya--;
         if(_input.down) ya++;
         if(_input.left) xa--;
         if(_input.right) xa++;
 
-        // TODO: nhớ cập nhật lại giá trị cờ _moving khi thay đổi trạng thái di chuyển
         if(xa != 0 || ya != 0){
             move(xa*Game.getBomberSpeed(), ya*Game.getBomberSpeed());
-            //autoMoveCenter();
             _moving =true;
         }
-        else    _moving = false;
+        else _moving = false;
     }
 
     @Override
     public boolean canMove(double x, double y) {
-        // @todo: kiểm tra có đối tượng tại vị trí chuẩn bị di chuyển đến và có thể di chuyển tới đó hay không
+        // TODO: kiểm tra có đối tượng tại vị trí chuẩn bị di chuyển đến và có thể di chuyển tới đó hay không
         int tileX = Coordinates.pixelToTile(x);
         int tileY = Coordinates.pixelToTile(y);
         Entity nextEntity = _board.getEntity(tileX, tileY, this);
         return collide(nextEntity);
-
-//        return true;
     }
 
     public void moveCenterX() {
-        int pixelOfEntity = Coordinates.tileToPixel(1);
+        int pixelOfEntity = Coordinates.tileToPixel(1); 
+		// vi tri Center dua vao vi tri dat bom trong ham detectPlaceBomb() (theo pixel)
         double centerX = _x + _sprite.getRealWidth() / 2;
         int tileCenterX = Coordinates.pixelToTile(centerX);
         _x = Coordinates.tileToPixel(tileCenterX) + pixelOfEntity / 2 - _sprite.getRealWidth() / 2;
@@ -167,6 +162,7 @@ public class Bomber extends Character {
 
     public void moveCenterY() {
         int pixelOfEntity = Coordinates.tileToPixel(1);
+		// vi tri Center dua vao vi tri dat bom trong ham detectPlaceBomb() (theo pixel)
         double centerY = _y - _sprite.getRealHeight() / 2;
         int tileCenterY = Coordinates.pixelToTile(centerY);
         _y = Coordinates.tileToPixel(tileCenterY) + pixelOfEntity / 2 + _sprite.getRealHeight() / 2;
@@ -182,54 +178,61 @@ public class Bomber extends Character {
         boolean contactLeft = !canMove(centerX - pixelOfEntity / 2, centerY);
         boolean contactRight = !canMove(centerX + pixelOfEntity / 2, centerY);
 
-        // Các trường hợp đi một nửa người vào tường cũng tự động căn giữa.
+        // Tu dong can giua khi di chuyen bi vuong nua nguoi
         if (_direction != 0 && contactDown) moveCenterY();
         if (_direction != 1 && contactLeft) moveCenterX();
         if (_direction != 2 && contactTop) moveCenterY();
         if (_direction != 3 && contactRight) moveCenterX();
     }
-
+	
     @Override
     public void move(double xa, double ya) {
-        // @todo: sử dụng canMove() để kiểm tra xem có thể di chuyển tới điểm đã tính toán hay không và thực hiện thay đổi tọa độ _x, _y
-        // @todo: nhớ cập nhật giá trị _direction sau khi di chuyển : up, right, down, left -> 0, 1, 2, 3
-        // @todo: Di chuyển nhân vật ra giữa.
-
-        // Tính tọa độ tâm người
+        // TODO: sử dụng canMove() để kiểm tra xem có thể di chuyển tới điểm đã tính toán hay không và thực hiện thay đổi t�?a độ _x, _y
+        // TODO: nhớ cập nhật giá trị _direction sau khi di chuyển: up, right, down, left -> 0, 1, 2, 3
+		// TODO: Di chuyển nhân vật ra giữa
+		
+		// Tinh toa do tam bomber
         double centerX = _x + _sprite.getRealWidth() / 2;
         double centerY = _y - _sprite.getRealHeight() / 2;
-
+		// cao nhat gia tri _direction khi di chuyen
         if (xa > 0) _direction = 1;
         if (xa < 0) _direction = 3;
         if (ya > 0) _direction = 2;
         if (ya < 0) _direction = 0;
+		// kiem tra xem co the di chuyen den vi tri da tinh toan hay khong va thuc hien thay doi toa do _x, _y
         if (canMove(centerX + xa, centerY + ya)) {
             _x += xa;
             _y += ya;
         }
-
+		// di chuyen nhan vat ra giua
         autoMoveCenter();
     }
 
     @Override
     public boolean collide(Entity e) {
-        // @todo: xử lý va chạm với Flame
-        // @todo: xử lý va chạm với Enemy
+        // TODO: xử lý va chạm với Flame
+        // TODO: xử lý va chạm với Enemy
+		
+        // tra ve true: khong co va cham
+        // tra ve false: co va cham
         if (e instanceof Flame) {
-            this.kill();
-            return true;
+        this.kill();
+        //bomber chet, tia lua bi vi tri cuoi cung cua bomber chan lai
+            return false;
         }
 
         if (e instanceof Enemy) {
             this.kill();
+            // bomber chet, enemy co the di qua vi tri cuoi cung cua bomber
             return true;
-        }
+        } //*
+		
+        if (e instanceof Wall) return false; // wall
 
-        if (e instanceof Wall) return false;
-
-        if (e instanceof LayeredEntity) return e.collide(this);
-
-        return true;
+        if (e instanceof LayeredEntity) return e.collide(this); // brick and item
+        return true; // grass
+        // return e.collide(this);
+        // neu ta chi dung lenh nay sau //* thi bomber khong di chuyen duoc sau khi dat bomb
     }
 
     private void chooseSprite() {
